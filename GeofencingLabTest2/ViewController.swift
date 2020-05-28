9408c73f2f9f19963f7c058ff49568d7bfa36700
Louise Chan <lbchan525@gmail.com>
2020-05-28 18:14:27 -0400

    Initial Commit

---
+++ GeofencingLabTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeofencingLabTest2
+//
+//  Created by Louise Chan on 2020-05-28.
+//  Copyright © 2020 Louise Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
